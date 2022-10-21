5ded5bd82342ffa25e520061c71f1da657846a13
Azzaulin <azzaulin@gmail.com>
2022-10-21 09:59:39 +0800

    upup

--- assets/variables.js
+++ assets/variables.js
@@ -1,5 +1,5 @@
 export default {
-  nicknames: ["Intan", "Tann", "Intunnn", "Intannnn"],
+  nicknames: ["Gloriaaa", "Glooo", "Ayang Gloooo",],
   greetings: {
     evening: "Good Evening",
     afternoon: "Good Afternoon",
